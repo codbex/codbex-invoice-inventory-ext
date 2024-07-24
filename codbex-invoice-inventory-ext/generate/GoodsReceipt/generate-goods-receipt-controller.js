8de7026574ead082ef939be00193ec035c794557
MinaDoncheva <133380007+MinaDoncheva@users.noreply.github.com>
2024-07-24 14:26:12 +0300

    create extension

---
+++ codbex-invoice-inventory-ext/generate/GoodsReceipt/generate-goods-receipt-controller.js
@@ -0,0 +1,69 @@
+const app = angular.module('templateApp', ['ideUI', 'ideView']);
+app.controller('templateController', ['$scope', '$http', 'ViewParameters', 'messageHub', function ($scope, $http, ViewParameters, messageHub) {
+    const params = ViewParameters.get();
+    $scope.showDialog = true;
+
+    const purchaseOrderDataUrl = "/services/ts/codbex-invoice-inventory-ext/generate/GoodsReceipt/api/GenerateGoodsReceiptService.ts/purchaseOrderData/" + params.id;
+    $http.get(purchaseOrderDataUrl)
+        .then(function (response) {
+            $scope.PurchaseOrderData = response.data;
+        });
+
+    const purchaseOrderItemsUrl = "/services/ts/codbex-invoice-inventory-ext/generate/GoodsReceipt/api/GenerateGoodsReceiptService.ts/purchaseOrderItemsData/" + params.id;
+    $http.get(purchaseOrderItemsUrl)
+        .then(function (response) {
+            $scope.PurchaseOrderItemsData = response.data;
+        });
+
+    $scope.generateGoodsReceipt = function () {
+        const goodsReceiptUrl = "/services/ts/codbex-inventory/gen/api/GoodsReceipts/GoodsReceiptService.ts/";
+        const entity = {
+            "Date": $scope.PurchaseOrderData.Date,
+            "Net": $scope.PurchaseOrderData.Net,
+            "Company": $scope.PurchaseOrderData.Company,
+            "Currency": $scope.PurchaseOrderData.Currency,
+            "Gross": $scope.PurchaseOrderData.Gross,
+            "VAT": $scope.PurchaseOrderData.VAT,
+            "Reference": $scope.PurchaseOrderData.UUID
+        };
+
+        $http.post(goodsReceiptUrl, entity)
+            .then(function (response) {
+                $scope.GoodsReceipt = response.data
+
+                if (!angular.equals($scope.OrderItems, {})) {
+                    $scope.PurchaseOrderItemsData.forEach(orderItem => {
+                        const goodsReceiptItem = {
+                            "GoodsReceipt": $scope.GoodsReceipt.Id,
+                            "Product": orderItem.Product,
+                            "Quantity": orderItem.Quantity,
+                            "UoM": orderItem.UoM,
+                            "Price": orderItem.Price,
+                            "Net": orderItem.Net,
+                            "VAT": orderItem.VAT,
+                            "Gross": orderItem.Gross
+                        };
+                        const goodsReceiptItemUrl = "/services/ts/codbex-inventory/gen/api/GoodsReceipts/GoodsReceiptItemService.ts/"
+                        $http.post(goodsReceiptItemUrl, goodsReceiptItem)
+                    });
+                }
+
+                console.log("GoodsReceipt created successfully:", response.data);
+                //alert("GoodsReceipt created successfully");
+            })
+            .catch(function (error) {
+                console.error("Error creating GoodsReceipt:", error);
+                //alert("Error creating purchase GoodsReceipt: ");
+            });
+
+        $scope.closeDialog();
+    };
+
+    $scope.closeDialog = function () {
+        $scope.showDialog = false;
+        messageHub.closeDialogWindow("goods-receipt-generate");
+    };
+
+    // Display the dialog when the page loads
+    document.getElementById("dialog").style.display = "block";
+}]);
---
+++ codbex-invoice-inventory-ext/generate/GoodsReceipt/goods-receipt-generate.js
@@ -0,0 +1,15 @@
+const viewData = {
+    id: 'goods-receipt-generate',
+    label: 'Generate Goods Receipt',
+    link: '/services/web/codbex-invoice-inventory-ext/generate/GoodsReceipt/generate-goods-receipt.html',
+    perspective: 'PurchaseOrder',
+    view: 'PurchaseOrder',
+    type: 'entity',
+    order: 20
+};
+
+if (typeof exports !== 'undefined') {
+    exports.getDialogWindow = function () {
+        return viewData;
+    }
+}
